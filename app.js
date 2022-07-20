fc4848a6440c88deea0d058d0200a67456ecf129
syz <syz@MacBook-Pro.RT-AC88U>
2022-07-20 15:13:27 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const datapipe = require('./datapipe');
+const BilibiliSocket = require("./bilibili-socket")
+!function () {
+    //房间号
+    let bilibiliClient = new BilibiliSocket(25143941);
+    //端口
+    let pipe = new datapipe(10086);
+    //协议监听
+    bilibiliClient.onOpen = async function () {
+        console.log(">>>>>>>>>>>>>> link room success!!!");
+        console.log(">>>>>>>>>>>>>> start to receive danmaku!!!  roomid: " + bilibiliClient.roomId);
+    };
+    bilibiliClient.onClose = async function (e) {
+        console.log(">>>>>>>>>>>>>> broken link...");
+    }
+    bilibiliClient.onError = async function (e) {
+        console.log(`>>>>>>>>>>>>>> unknown error: \n${e.message}`);
+    }
+    bilibiliClient.onPopularityChange = async function (value) {
+        pipe.top(value);
+    }
+    bilibiliClient.onMessage = async function (msg) {
+        pipe.danmaku(msg);
+    }
+    bilibiliClient.connect();
+}();
